9cfed702f70dcb07190c7affe70229f19374d94d
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-11-19 00:49:27 +0500

    add placeholder to the checkout page.

--- functions.php
+++ functions.php
@@ -70,7 +70,6 @@ add_filter( 'woocommerce_default_address_fields', 'custom_override_default_local
 
 
 
-// Adding placeholder
 
 
 add_filter( 'woocommerce_checkout_fields' , 'override_billing_checkout_fields', 20, 1 );
@@ -84,9 +83,17 @@ function override_billing_checkout_fields( $fields ) {
     
     $fields['billing']['billing_email']['placeholder'] = 'Email';
 
+    $fields['billing']['billing_phone']['placeholder'] = 'Phone Number';
+
     $fields['billing']['billing_business_name']['placeholder'] = 'Business Name';
 
     $fields['billing']['billing_business_address']['placeholder'] = 'Business Address';
+
+    $fields['billing']['billing_job_title']['placeholder'] = 'Job Title';
+
+    $fields['billing']['billing_project_name']['placeholder'] = 'Project Name';
+    
+    $fields['billing']['billing_about_us']['placeholder'] = 'How to did you hear about us?';
 
     return $fields;
 
