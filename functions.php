75ab2bf8f492b5d29ff82b4ccc1c1fef9dcb360c
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-11-24 11:41:19 +0500

    add all new required fields.

--- functions.php
+++ functions.php
@@ -129,10 +129,49 @@ add_filter( 'woocommerce_checkout_fields' , 'job_title_custom_override_checkout_
 
 function job_title_custom_override_checkout_fields( $fields ) {
 
-    $fields['billing']['billing_business_name'] = array(
+    $fields['billing']['billing_job_title'] = array(
 
-        'label'     => __('Business Name', 'woocommerce'),
-        'placeholder'   => _x('Business Name', 'placeholder', 'woocommerce'),
+        'label'     => __('Job Title', 'woocommerce'),
+        'placeholder'   => _x('Job Title', 'placeholder', 'woocommerce'),
+        'required'  => false,
+        'class'     => array('form-row-wide'),
+        'clear'     => true
+
+    );
+
+    return $fields;
+}
+
+/**
+ * Display field value on the order edit page
+ */
+ 
+add_action( 'woocommerce_admin_order_data_after_shipping_address', 'job_custom_checkout_field_display_admin_order_meta', 20, 2 );
+
+function job_custom_checkout_field_display_admin_order_meta($order){
+
+    echo '<p><strong>'.__('Job Title From Checkout Form').':</strong> ' . get_post_meta( $order->get_id(), '_billing_job_title', true ) . '</p>';
+
+}
+
+
+/*
+    ==================================================
+      Adding custom field == Project Name
+    ==================================================
+
+*/
+
+add_filter( 'woocommerce_checkout_fields' , 'project_title_custom_override_checkout_fields' );
+
+// Our hooked in function - $fields is passed via the filter!
+
+function project_title_custom_override_checkout_fields( $fields ) {
+
+    $fields['billing']['billing_Project_name'] = array(
+
+        'label'     => __('Project Name', 'woocommerce'),
+        'placeholder'   => _x('Project Name', 'placeholder', 'woocommerce'),
         'required'  => true,
         'class'     => array('form-row-wide'),
         'clear'     => true
@@ -146,54 +185,54 @@ function job_title_custom_override_checkout_fields( $fields ) {
  * Display field value on the order edit page
  */
  
-add_action( 'woocommerce_admin_order_data_after_shipping_address', 'job_custom_checkout_field_display_admin_order_meta', 10, 1 );
+add_action( 'woocommerce_admin_order_data_after_shipping_address', 'project_custom_checkout_field_display_admin_order_meta', 30, 1 );
 
-function job_custom_checkout_field_display_admin_order_meta($order){
+function project_custom_checkout_field_display_admin_order_meta($order){
 
-    echo '<p><strong>'.__('Bussiness Name From Checkout Form').':</strong> ' . get_post_meta( $order->get_id(), '_billing_business_name', true ) . '</p>';
-    
+    echo '<p><strong>'.__('Project Name From Checkout Form').':</strong> ' . get_post_meta( $order->get_id(), '_billing_project_name', true ) . '</p>';
+
 }
 
 
+/*
+    ==================================================
+      Adding custom field == About us
+    ==================================================
+
+*/
+
+add_filter( 'woocommerce_checkout_fields' , 'about_title_custom_override_checkout_fields' );
+
+// Our hooked in function - $fields is passed via the filter!
+
+function about_title_custom_override_checkout_fields( $fields ) {
+
+    $fields['billing']['billing_about_us'] = array(
+
+        'label'     => __('How did you hear about us?', 'woocommerce'),
+        'placeholder'   => _x('How did you hear about us?', 'placeholder', 'woocommerce'),
+        'required'  => true,
+        'class'     => array('form-row-wide'),
+        'clear'     => true
+
+    );
+
+    return $fields;
+}
+
+/**
+ * Display field value on the order edit page
+ */
+ 
+add_action( 'woocommerce_admin_order_data_after_shipping_address', 'about_custom_checkout_field_display_admin_order_meta', 30, 1 );
+
+function about_custom_checkout_field_display_admin_order_meta($order){
+
+    echo '<p><strong>'.__('How did you hear about us Name From Checkout Form').':</strong> ' . get_post_meta( $order->get_id(), '_billing_about_us', true ) . '</p>';
+
+}
 
 
-
-
-
-
-
-
-
-// Woocommerce checkout field order changed
-
-// add_filter("woocommerce_checkout_fields", "custom_override_checkout_fields", 1);
-
-// function custom_override_checkout_fields($fields) {
-
-//     $fields['billing']['billing_first_name']['priority'] = 1;
-//     $fields['billing']['billing_last_name']['priority'] = 2;
-//     $fields['billing']['billing_email']['priority'] = 3;
-//     $fields['billing']['billing_phone']['priority'] = 4;
-//     $fields['billing']['billing_business_name']['priority'] = 5;
-//     $fields['billing']['billing_address_1']['priority'] = 5;
-//     $fields['billing']['billing_job_title']['priority'] = 7;
-//     $fields['billing']['billing_project_name']['priority'] = 8;
-//     $fields['billing']['billing_about_us']['priority'] = 9;
-
-//     // First Name & Last Name
-
-//     $fields['billing']['billing_first_name']['class'][0] = 'form-row-first';
-//     $fields['billing']['billing_last_name']['class'][0] = 'form-row-last';
-
-//     // Business Name and Number
-
-//     $fields['billing']['billing_business_name']['class'][0] = 'form-row-first';
-//     $fields['billing']['billing_business_address']['class'][0] = 'form-row-last';
-
-//     return $fields;
-// }
-
-// add_filter( 'woocommerce_default_address_fields', 'custom_override_default_locale_fields' );
 
 
 
