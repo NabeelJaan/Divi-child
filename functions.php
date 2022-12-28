546380d879151152c078afcd4452ec94cba5a97e
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-12-29 04:15:08 +0500

    webp images
    added snippet into function.php to allow server to upload the webp images format

--- functions.php
+++ functions.php
@@ -432,3 +432,42 @@ function woocommerce_support() {
 // }
 
 // add_action('woocommerce_after_order_notes', 'cw_custom_checkbox_fields');
+
+
+
+/*
+    ===================================
+      Adding checkbox on checkout form
+    ===================================
+*/
+
+
+function webp_upload_mimes( $existing_mimes ) {
+    // add webp to the list of mime types
+    $existing_mimes['webp'] = 'image/webp';
+    // return the array back to the function with our added mime type
+    return $existing_mimes;
+}
+
+add_filter( 'mime_types', 'webp_upload_mimes' );
+
+//** * Enable preview / thumbnail for webp image files.*/
+
+
+function webp_is_displayable($result, $path) {
+    if ($result === false) {
+        $displayable_image_types = array( IMAGETYPE_WEBP );
+        $info = @getimagesize( $path );
+        if (empty($info)) {
+            $result = false;
+        } elseif (!in_array($info[2], $displayable_image_types)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+    }
+    return $result;
+}
+
+
+add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
